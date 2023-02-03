1e4a7c3f31d03a360f7133a4d7bb55e0b0d9298d
awatanabe <awatanabe@student-bmhs-la.org>
2023-02-03 12:26:57 -0800

    final.

--- Game.java
+++ Game.java
@@ -1,8 +1,11 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Game {
 
     private String hidden;
     private String guess;
     private String hint;
+    Scanner kb = new Scanner(System.in);
 
     public static void main(String[] args) {
         Game game = new Game();
@@ -10,7 +13,32 @@ public class Game {
     }
 
     public void run() {
-            System.out.println("hello");
+
+        System.out.println("Enter a hidden word with 5 letters.");
+        hidden = kb.nextLine();
+
+         while(!Objects.equals(guess, hidden)){
+             hint = "";
+             System.out.println("Enter a 5 letter guess.");
+             guess = kb.nextLine();
+
+            for(int i=0; i<hidden.length(); i++){
+                if(hidden.substring(i, i+1).equals(guess.substring(i, i+1)))  {
+                   hint += guess.substring(i, i+1);
+                }
+                else if (hidden.indexOf(guess.substring(i,i+1)) != -1){
+                    hint +=("*");
+                }
+                else{
+                    hint += "+";
+                }
+
+
+            }
+             System.out.println(hint);
+        }
+System.out.print("You got it!");
+
     }
 
 }
